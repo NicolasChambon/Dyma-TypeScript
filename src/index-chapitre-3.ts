df4bbeb644bd8d37bbafbf61b0a150e02723c98c
NicolasChambon <nico.chambon@gmail.com>
2024-09-19 23:06:41 +0200

    chapitre 4 - les classes. Done

---
+++ src/index-chapitre-3.ts
@@ -0,0 +1,35 @@
+// let sumTwoNb: (nb1: number, nb2: number) => number;
+
+// sumTwoNb = (a: number, b?: number) => {
+//   if (b) {
+//     return a + b;
+//   }
+//   return a;
+// };
+
+//---------------
+
+// let sumTwoNb = (...numbers: number[]) => { // ...param est forcemment un tableau
+//   return numbers.reduce((acc, number) => { // number est la valeur courante du tableau
+//     acc += number;
+//     return acc;
+//   }, 0);
+// };
+
+// sumTwoNb(1, 2, 3, 4);
+
+//---------------
+
+// function addition(a: number, b: number): number;
+// function addition(a: string, b: string): string;
+// function addition(a, b): string | number {
+//   return a + b;
+// }
+
+//---------------
+
+// const foo = addition(1, 2);
+// const bar = addition('hello ', 'world');
+
+// bar.split('');
+// bar.length;
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,79 @@
-// let sumTwoNb: (nb1: number, nb2: number) => number;
+abstract class Engine  {
+  constructor(protected type: string){}
 
-// sumTwoNb = (a: number, b?: number) => {
-//   if (b) {
-//     return a + b;
-//   }
-//   return a;
-// };
+  abstract stopEngine(): number;
 
-// let sumTwoNb = (...numbers: number[]) => { // ...param est forcemment un tableau
-//   return numbers.reduce((acc, number) => { // number est la valeur courante du tableau
-//     acc += number;
-//     return acc;
-//   }, 0);
-// };
-
-// sumTwoNb(1, 2, 3, 4);
-
-function addition(a: number, b: number): number;
-function addition(a: string, b: string): string;
-function addition(a, b): string | number {
-  return a + b;
 }
 
-const foo = addition(1, 2);
-const bar = addition('hello ', 'world');
+class Vehicule extends Engine {
+  wheel = 4;
 
-bar.split('');
-bar.length;
+  protected brand: string;
+
+  constructor(brand: string) {
+    super('v8');
+    this.brand = brand;
+  }
+
+  stopEngine() {
+    console.log('stop engine');
+    return 0;
+  }
+}
+
+
+class Voiture extends Vehicule {
+  static className = 'Vehicule';
+
+  static startCar() {
+    console.log('car can start');
+    
+  }
+
+  private maxSpeed: number = 100;
+  readonly airbag: boolean;
+
+  move(): void {
+    console.log('car move');
+  }
+
+  faster(newSpeed: number): void {
+    if (newSpeed < this.maxSpeed) {
+      this.speed = newSpeed;
+    }
+  }
+
+  stop(): void {
+    console.log('car stop');
+  }
+
+  changeBrand(newBrand: string): void {
+    this.brand = newBrand;
+  }
+
+  constructor(public speed: number) {
+    super('dacia');
+    this.airbag = true;
+    this.speed = speed;
+  }
+}
+
+let car: Voiture;
+car = new Voiture(30);
+// car.maxSpeed = 200;
+// car.brand = 'tesla';
+// car.changeBrand('citroen');
+
+// console.log('car.wheel', car.wheel);
+// console.log('car.maxSpeed', car.maxSpeed);
+console.log(Voiture.className);
+console.log(Voiture.startCar());
+
+
+console.log('car', car);
+
+
+
+
+//---------------
+
