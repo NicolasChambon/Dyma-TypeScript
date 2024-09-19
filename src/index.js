301593cbfd912f6bc4466498b056a1e946d5bfcf
NicolasChambon <nico.chambon@gmail.com>
2024-09-19 21:16:42 +0200

    chapitre 3 - les fonctions. Done

--- src/index.js
+++
@@ -1,5 +0,0 @@
-var foo = 'une string'; // ou "une string" // `une ${'string'}`
-var bar = 0; // ou : 5.5 // -1.2 // 10e2
-var boolean = true; // ou false // 1 === 1;
-var obj = []; // new Map // {name: 'nico' }
-var anything = {}; // '123'
--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,29 @@
-const foo: string = 'une string'; // "une string"; // `une ${ 'string' }`;
-const bar: number = 0; // 5.5; // -1.2; // 10e2;
-const myBool: boolean = true; // false; // 1 === 1;
+// let sumTwoNb: (nb1: number, nb2: number) => number;
 
-const myObj: object = []; // new Map(); // { name: 'nico' };
-const anything: any = {}; // '123';
-const myConst: 5 = 5; // le type est la valeur elle même, il n'y a pas d'autre possibilité
+// sumTwoNb = (a: number, b?: number) => {
+//   if (b) {
+//     return a + b;
+//   }
+//   return a;
+// };
 
-const myArr: number[] = [1, 2, 3];
-const myArr1: Array<string> = ['un', 'deux', 'trois'];
-const tuple: [number, string] = [1, 'deux'];
+// let sumTwoNb = (...numbers: number[]) => { // ...param est forcemment un tableau
+//   return numbers.reduce((acc, number) => { // number est la valeur courante du tableau
+//     acc += number;
+//     return acc;
+//   }, 0);
+// };
 
-enum Couleur {
-  Bleu = 10,
-  Blanc,
-  Rouge = 5
-};
+// sumTwoNb(1, 2, 3, 4);
 
-let a: Couleur = Couleur.Bleu;
-let b: Couleur = Couleur.Blanc;
-let c: Couleur = Couleur.Rouge;
-
-console.log(a); // 10
-console.log(b); // 11
-console.log(c); // 5
-
-enum Couleur2 {
-  Bleu = '#2980b9',
-  Vert = "#27ae60",
-  Rouge = '#c0392b'
-};
-
-console.log(Couleur2.Vert); // #27ae60
-
-const foo2: null = null;
-const foo3: undefined = undefined;
-const foo4: void = undefined; // undefined;
-
-function add(nb1: number, nb2: number): void {
-  console.log(nb1 + nb2);
+function addition(a: number, b: number): number;
+function addition(a: string, b: string): string;
+function addition(a, b): string | number {
+  return a + b;
 }
 
-function error(message: string): never {
-  throw new Error(message);
-}
+const foo = addition(1, 2);
+const bar = addition('hello ', 'world');
 
-function echec(): never {
-  return error('Problème !')
-}
-
-function boucleInfinie(): never {
-  while(true) {
-
-  }
-}
-
-let username: any;
-username = 'jean';
-
-// const nbr: number = (<string>username).length;
-const nbr: number = (username as string).length;
+bar.split('');
+bar.length;
