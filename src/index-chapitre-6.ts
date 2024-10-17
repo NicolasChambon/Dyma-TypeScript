0084a265c1d51c2b7184c4ae740741f3dd973e78
NicolasChambon <nico.chambon@gmail.com>
2024-10-17 16:36:03 +0200

    Generic Type (chapter 7) lesson 2 done

---
+++ src/index-chapitre-6.ts
@@ -0,0 +1,175 @@
+// let userStatus: 'online' | 'offline' | 'busy' = 'online';
+
+// let value: number | string;
+// value = 5;
+// value = 'hello';
+// // value = true; // Error: Type 'boolean' is not assignable to type 'number | string'.
+
+// interface Car {
+//   category: 'car';
+//   seat: number;
+//   drive: () => void;
+// };
+
+// interface Truck {
+//   category: 'truck';
+//   seat: number;
+//   load: (amount: number) => void;
+//   drive: () => void;
+// };
+
+// const myVehicle: Car | Truck = {
+//   category: 'truck',
+//   seat: 4,
+//   load: (amount: number): void => {},
+//   drive: () => {}
+// };
+
+// function vehiculeFactory(type: 1 | 2): Car | Truck {
+//   if (type === 1) {
+//     return {
+//       category: 'car',
+//       seat: 4,
+//       drive: () => {}
+//     };
+//   } else {
+//     return {
+//       category: 'truck',
+//       seat: 2,
+//       load: (amount: number): void => {},
+//       drive: () => {}
+//     };
+//   }
+// }
+
+// const foo = vehiculeFactory(2);
+
+// if (foo.category === 'truck') {
+//   foo.load(5);
+// }
+
+// function startTrip(v: Car | Truck) {
+//   v.drive ();
+//   switch (v.category) {
+//     case 'car':
+//       break;
+//     case 'truck':
+//       v.load(5);
+//       break;
+//   }
+// }
+
+// interface Bird {
+//   fly(): void;
+// }
+
+// interface Whale {
+//   swim(): void;
+// }
+
+// function isBird(animal: Bird | Whale): animal is Bird {
+//   return (animal as Bird).fly !== undefined;
+// }
+
+// function move(animal: Bird | Whale) {
+//   if (isBird(animal)) {
+//     animal.fly();
+//   } else {
+//     animal.swim();
+//   }
+// }
+  
+// function move(animal: Bird | Whale) {
+//   if ('fly' in animal) {
+//     animal.fly();
+//   } else {
+//     animal.swim();
+//   }
+// }
+
+// function foo(a: string | number) {
+//   if (typeof a === 'number') {
+//     a.toFixed();
+//   }
+// }
+
+// class A {
+//   getA(){}
+// }
+
+// class B {
+//   getB(){}
+// }
+
+// const a = new A();
+// const b = new B();
+
+// function bar(x: A | B) {
+//   if (x instanceof A) {
+//     x.getA();
+//   } else {
+//     x.getB();
+//   }
+// }
+
+// interface User {
+//   username: string;
+// }
+
+// interface Moderator {
+//   deleteMessage: () => void;
+//   editMessage: () => void;
+// }
+
+// interface AddContent {
+//   addMessage: () => void;
+// }
+
+// interface BasicUser extends User, AddContent {}
+// interface Admin extends User, AddContent, Moderator {}
+
+// let newUser: User & AddContent = {
+//   username: 'Paul',
+//   addMessage: () => {}
+// }
+
+// let admin: User & AddContent & Moderator = {
+//   username: 'Jean',
+//   addMessage: () => {},
+//   deleteMessage: () => {},
+//   editMessage: () => {}
+// }
+
+// type customModulos = 1 | 2 | 3;
+
+// type ObjectId = string;
+
+// type User = {
+//   username?: string;
+//   age: number;
+//   address?: {
+//     city: string;
+//   }
+// };
+
+// const myUser: User = {
+//   username: 'Jean',
+//   age: 30
+// };
+
+// const city = myUser.address?.city;
+
+// const username = myUser.username || 'guest'; // if username is null or undefined then 'guest' is used
+// const username = myUser.username ?? 'guest'; // if username is null or undefined then 'guest' is used but not if username is 0 or false of empty string
+
+
+// type UserStatusType = 'online' | 'offline' | 'busy';
+// type customModulos = 1 | 2 | 3;
+
+// type 
+
+// let userStatus: UserStatusType = 'online';
+
+// function getUserStatus(user): UserStatusType {
+//   return user.status;
+// }
--- src/index.ts
+++ src/index.ts
@@ -1,172 +1,48 @@
-// let userStatus: 'online' | 'offline' | 'busy' = 'online';
+// const arr: string[] = ['pomme', 'banane', 'orange'];
+const arr: Array<string> = ['pomme', 'banane', 'orange'];
 
-// let value: number | string;
-// value = 5;
-// value = 'hello';
-// // value = true; // Error: Type 'boolean' is not assignable to type 'number | string'.
+const fruit = arr[0];
 
-// interface Car {
-//   category: 'car';
-//   seat: number;
-//   drive: () => void;
-// };
-
-// interface Truck {
-//   category: 'truck';
-//   seat: number;
-//   load: (amount: number) => void;
-//   drive: () => void;
-// };
-
-// const myVehicle: Car | Truck = {
-//   category: 'truck',
-//   seat: 4,
-//   load: (amount: number): void => {},
-//   drive: () => {}
-// };
-
-// function vehiculeFactory(type: 1 | 2): Car | Truck {
-//   if (type === 1) {
-//     return {
-//       category: 'car',
-//       seat: 4,
-//       drive: () => {}
-//     };
-//   } else {
-//     return {
-//       category: 'truck',
-//       seat: 2,
-//       load: (amount: number): void => {},
-//       drive: () => {}
-//     };
-//   }
-// }
-
-// const foo = vehiculeFactory(2);
-
-// if (foo.category === 'truck') {
-//   foo.load(5);
-// }
-
-// function startTrip(v: Car | Truck) {
-//   v.drive ();
-//   switch (v.category) {
-//     case 'car':
-//       break;
-//     case 'truck':
-//       v.load(5);
-//       break;
-//   }
-// }
-
-// interface Bird {
-//   fly(): void;
-// }
-
-// interface Whale {
-//   swim(): void;
-// }
-
-// function isBird(animal: Bird | Whale): animal is Bird {
-//   return (animal as Bird).fly !== undefined;
-// }
-
-// function move(animal: Bird | Whale) {
-//   if (isBird(animal)) {
-//     animal.fly();
-//   } else {
-//     animal.swim();
-//   }
-// }
-  
-// function move(animal: Bird | Whale) {
-//   if ('fly' in animal) {
-//     animal.fly();
-//   } else {
-//     animal.swim();
-//   }
-// }
-
-// function foo(a: string | number) {
-//   if (typeof a === 'number') {
-//     a.toFixed();
-//   }
-// }
-
-// class A {
-//   getA(){}
-// }
-
-// class B {
-//   getB(){}
-// }
-
-// const a = new A();
-// const b = new B();
-
-// function bar(x: A | B) {
-//   if (x instanceof A) {
-//     x.getA();
-//   } else {
-//     x.getB();
-//   }
-// }
-
-// interface User {
-//   username: string;
-// }
-
-// interface Moderator {
-//   deleteMessage: () => void;
-//   editMessage: () => void;
-// }
-
-// interface AddContent {
-//   addMessage: () => void;
-// }
-
-// interface BasicUser extends User, AddContent {}
-// interface Admin extends User, AddContent, Moderator {}
-
-// let newUser: User & AddContent = {
-//   username: 'Paul',
-//   addMessage: () => {}
-// }
-
-// let admin: User & AddContent & Moderator = {
-//   username: 'Jean',
-//   addMessage: () => {},
-//   deleteMessage: () => {},
-//   editMessage: () => {}
-// }
-
-// type customModulos = 1 | 2 | 3;
-
-// type ObjectId = string;
-
-// type User = {
-//   username?: string;
-//   age: number;
-//   address?: {
-//     city: string;
-//   }
-// };
-
-// const myUser: User = {
-//   username: 'Jean',
-//   age: 30
-// };
-
-// const city = myUser.address?.city;
-
-// const username = myUser.username || 'guest'; // if username is null or undefined then 'guest' is used
-// const username = myUser.username ?? 'guest'; // if username is null or undefined then 'guest' is used but not if username is 0 or false of empty string
-
-
-type UserStatusType = 'online' | 'offline' | 'busy';
-
-let userStatus: UserStatusType = 'online';
-
-function getUserStatus(user): UserStatusType {
-  return user.status;
+interface User {
+  username: string;
 }
+
+const promise: Promise<User> = new Promise((resolve, reject) => {
+  resolve({ username: 'toto' });
+});
+
+promise.then((res) => {
+  res.username;
+});
+
+interface Fruit {
+  name: string;
+  price: number;
+}
+
+interface Car {
+  speed: number;
+}
+
+const newCar: Car = {
+  speed: 50,
+};
+
+const newFruit: Fruit = {
+  name: 'pomme',
+  price: 1,
+};
+
+interface AddItemFunction {
+  <T>(item: T, collection: T[]): T[];
+}
+
+const addItemToCollection: AddItemFunction = (item, collection) => {
+  return [...collection, item];
+}
+
+const myFruits = addItemToCollection(newFruit, []);
+const myCars = addItemToCollection(newCar, []);
+
+console.log(myFruits[0].price);
+console.log(myCars[0].speed);
