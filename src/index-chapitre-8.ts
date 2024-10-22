5694a6d77c13bc6825be82ca5c189fd7827c9efe
NicolasChambon <nico.chambon@gmail.com>
2024-10-22 10:34:45 +0200

    chapter 8 done

---
+++ src/index-chapitre-8.ts
@@ -0,0 +1,29 @@
+// import { Shape } from './shape';
+// import { add } from '../mylib/add';
+// import $ from 'jquery';
+
+// declare const Swal: any;
+
+// const circle = new Shape.Circle();
+// const square = new Shape.Square();
+
+// const myH1 = $('h1');
+
+// myH1.click((e) => {
+//   console.log('clicked');
+// });
+
+// console.log(myH1);
+
+// console.log(circle);
+// console.log(square);
+
+// setTimeout(() => {
+//   Swal.fire("Good job!", "You clicked the button!", "success");
+// }, 3000);
+
+// console.log(add(1, 2));
+
+// const res = $('h1');
+
+// console.log(res);
